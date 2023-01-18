98d6e4ffac7881c28d1855a3ec87fb71c731f17e
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-01-17 23:49:56 -0300

    Feito documentação pelo swagger e implementado o tsyring

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,17 +1,18 @@
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import { inject, injectable } from "tsyringe";
 
 interface IRequest {
     name: string;
     description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {}
+    constructor(@inject("CategoriesRepository")
+        private categoriesRepository: ICategoriesRepository) { }
 
     async execute({ description, name }: IRequest): Promise<void> {
-
         const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
-        
         if (categoryAlreadyExists) {
             throw new Error("Category Already Exists!");
         }   
