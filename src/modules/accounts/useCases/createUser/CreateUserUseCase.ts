ad18bf543af3fc7813028004e51920d3e08cb932
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-01-24 22:04:59 -0300

    Criado os casos de teste, feito refatoração, e organização das pastas e imports

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -2,28 +2,28 @@ import { inject, injectable } from "tsyringe";
 import { hash } from "bcrypt";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
-import { AppError } from "../../../../erros/AppError";
+import { AppError } from "../../../../shared/erros/AppError";
 
 @injectable()
 class CreateUserUseCase {
-    constructor(@inject("UsersRepository") private usersRepository: IUsersRepository){}
+  constructor(@inject("UsersRepository") private usersRepository: IUsersRepository) { }
 
-    async execute({ name, email, password, driver_license }: ICreateUserDTO): Promise<void> {
+  async execute({name,email,password,driver_license}: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
-        const userAlreadyExists = await this.usersRepository.findByEmail(email)
-        if (userAlreadyExists) {
-            throw new AppError("User already exists");
-        }
-
-        const passwordHash = await hash(password, 8);
-
-        await this.usersRepository.create({
-            name,
-            email,
-            password: passwordHash,
-            driver_license
-        })
+    if (userAlreadyExists) {
+      throw new AppError("User already exists");
     }
+
+    const passwordHash = await hash(password, 8);
+    
+    await this.usersRepository.create({
+      name,
+      email,
+      password: passwordHash,
+      driver_license
+    })
+  }
 }
 
-export { CreateUserUseCase }
+export { CreateUserUseCase };
