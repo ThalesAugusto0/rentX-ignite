7247da0daaa48b7deb2d9976526a4bc328a9730f
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-02-01 00:26:50 -0300

    Criado upload de imagens dos carros

---
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -0,0 +1,20 @@
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { inject, injectable } from "tsyringe";
+
+interface IRequest {
+    car_id: string;
+    images_name: string[];
+}
+
+@injectable()
+class UploadCarImagesUseCase {
+    constructor(@inject("CarsImagesRepository") private carsImagensRepository: ICarsImagesRepository ){}
+
+    async execute({ car_id, images_name }: IRequest): Promise<void> {
+        images_name.map(async (image) => {
+            await this.carsImagensRepository.create(car_id, image);
+        })
+    }
+}
+
+export { UploadCarImagesUseCase }
