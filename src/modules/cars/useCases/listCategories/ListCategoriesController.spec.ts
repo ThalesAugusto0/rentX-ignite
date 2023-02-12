dc1e63c83a3131e83536adb66c495d481d1e57fa
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-02-12 19:26:27 -0300

    Criado testes de integração

--- src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -27,7 +27,7 @@ describe("Create Category Controller", () => {
         await connection.close()
     })
     
-    it("should be able to create a new category", async () => {
+    it("should be able to list all categories", async () => {
         const responseToken = await request(app).post("/sessions").send({
             email: "admin@rentx.com.br",
             password: "admin",
@@ -35,25 +35,15 @@ describe("Create Category Controller", () => {
 
         const { token } = responseToken.body.token
 
-        const response = await request(app).post("/categories").send({
+        await request(app).post("/categories").send({
             name: "Category SuperTest",
             description: "Category SuperTest"
-        }).set({Authorization: `Bearer ${token}`})
+        }).set({ Authorization: `Bearer ${token}` })
+        
+        const response = await request(app).get("/categories")
+
         expect(response.status).toBe(201);
-    });
-
-        it("should not be able to create a new category with name exists", async () => {
-        const responseToken = await request(app).post("/sessions").send({
-            email: "admin@rentx.com.br",
-            password: "admin",
-        })
-
-        const { token } = responseToken.body.token
-
-        const response = await request(app).post("/categories").send({
-            name: "Category SuperTest",
-            description: "Category SuperTest"
-        }).set({Authorization: `Bearer ${token}`})
-        expect(response.status).toBe(400);
+        expect(response.body.length).toBe(1);
+        expect(response.body[0]).toHaveProperty("id");
     });
 });
