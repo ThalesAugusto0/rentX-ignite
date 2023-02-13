1428824ffe40f8736f8c1cae41bc49f574c249ef
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-02-12 23:01:57 -0300

    Criado a devolução de um aluguel useCase

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,7 +5,8 @@ import { Rental } from "../infra/typeorm/entities/Rental"
 interface IRentalRepository {
     findOpenRentalByCar(car_id: string): Promise<Rental>
     findOpenRentalByUser(user_id: string): Promise<Rental>
-    create( data: ICreateRentalDTO ): Promise<Rental>
+    create(data: ICreateRentalDTO): Promise<Rental>
+    findById(id: string): Promise<Rental>
 }
 
 export { IRentalRepository }
