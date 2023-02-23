da81907350906a3aeecfa2e8ffbfa71dcfb18556
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-02-22 21:26:58 -0300

    Criado a URL para o avatar do user, tanto local quando no servidor

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import uploadConfig from "../../../../config/upload";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
 
 const usersRoutes = Router();
 
@@ -11,9 +12,10 @@ const uploadAvatar = multer(uploadConfig)
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
-
-usersRoutes.patch("/avatar",ensureAuthenticated , uploadAvatar.single("avatar"), updateUserAvatarController.handle);
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle);
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes }
