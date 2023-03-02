d7c90cf724d84aae324ed340b5ed59420aef739e
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-03-02 01:02:52 -0300

    cors config

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -20,8 +20,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
 
-app.use(cors())
-app.use(router)
+app.use(cors());
+app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
