452a1676bc113abd193ff8e44a44a7a18443b2ac
ThalesAugusto0 <thalllesaugustos28@gmail.com>
2023-03-02 22:57:27 -0300

    config rate limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,15 +10,17 @@ import "../../container"
 import { AppError } from "../../erros/AppError";
 import upload from "@config/upload";
 import cors from "cors";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 
 createConnection()
 const app = express();
 
-app.use(express.json())
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(rateLimiter);
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
-app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
+app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
@@ -27,13 +29,13 @@ app.use((err: Error, request: Request, response: Response, next: NextFunction) =
     if (err instanceof AppError) {
         return response.status(err.statusCode).json({
             message: err.message
-        })
-    }
+        });
+    };
 
     return response.status(500).json({
         status: "error",
         message: `Internal server error - ${err.message}`,
-    })
-})
+    });
+});
 
 export { app }
---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,33 @@
+import redis from "redis";
+import { RateLimiterRedis } from "rate-limiter-flexible"
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "@shared/erros/AppError";
+
+const redisClient = redis.createClient({
+    legacyMode: true,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+    }
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'middleware',
+  points: 10,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+    request: Request,
+    response: Response,
+    next: NextFunction
+): Promise<void> {
+    try {
+        await limiter.consume(request.ip);
+
+        return next();
+    } catch (err) {
+        throw new AppError("Too many request", 429);        
+    }
+};
